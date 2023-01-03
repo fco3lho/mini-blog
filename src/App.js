0f0f32b43f96725258748608745c00135a3b2916
Felipe Campos <felipecampos50123@gmail.com>
2023-01-03 13:41:28 -0300

    Adicionando componentes de Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 //Pages
 import Home from './pages/Home/Home.js'
 import About from './pages/About/About.js'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <div className="container">
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
