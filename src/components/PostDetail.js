3ddab0ec13b305c2a7e49238cf595922d59850ca
Felipe Campos <felipecampos50123@gmail.com>
2023-01-12 13:53:56 -0300

    CSS dos detalhes do post

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom"
 
 const PostDetail = ({post}) => {
   return (
-    <div>
-        <img src={post.image} alt={post.title} />
+    <div className={styles.post_detail}>
+        <div className={styles.bg_img}><img src={post.image} alt={post.title} /></div>
         <h2>{post.title}</h2>
-        <p>{post.createdBy}</p>
-        <div>
+        <p className={styles.createdby}>{post.createdBy}</p>
+        <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
                 <p key={tag}><span>#</span>{tag}</p>
             ))}
