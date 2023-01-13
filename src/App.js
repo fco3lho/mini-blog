b313f4ea29d16a3f7382fcf7fc59ca3f9f3a0dbb
Felipe Campos <felipecampos50123@gmail.com>
2023-01-13 14:44:14 -0300

    Criado página de post individual

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ import Login from './pages/Login/Login.js'
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard'
 import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { onAuthStateChanged } from 'firebase/auth';
+
 
 function App() {
   const [user, setUser] = useState(undefined)
@@ -47,6 +49,7 @@ function App() {
               <Route path="/" element={<Home/>}/>
               <Route path="/about" element={<About/>}/>
               <Route path="/search" element={<Search/>}/>
+              <Route path="/posts/:id" element={<Post/>}/>
               <Route path="/login" element={!user ? <Login/> : <Navigate to="/"/>}/>
               <Route path="/register" element={!user ? <Register/> : <Navigate to="/"/>}/>
               <Route path="/posts/create" element={user ? <CreatePost/> : <Navigate to="/login"/>}/>
